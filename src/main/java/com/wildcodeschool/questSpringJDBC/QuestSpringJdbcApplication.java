5a148be91d54dbd95adf1a59f128206f1a961b94
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-06-26 16:59:45 +0200

    TEST OK

---
+++ src/main/java/com/wildcodeschool/questSpringJDBC/QuestSpringJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.questSpringJDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestSpringJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestSpringJdbcApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/wildcodeschool/questSpringJDBC/controllers/SchoolController.java
@@ -0,0 +1,108 @@
+package com.wildcodeschool.questSpringJDBC.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@ResponseBody
+public class SchoolController {
+
+    private final static String DB_URL = "jdbc:mysql://localhost:3306/wild_db_quest?serverTimezone=GMT";
+    private final static String DB_USER = "root";
+    private final static String DB_PASSWORD = "@ps745GF";
+
+    @GetMapping("/api/schools")
+    public List<School> getSchools(@RequestParam(defaultValue = "%") String country) {
+
+        try(
+            Connection connection = DriverManager.getConnection( 
+                DB_URL, DB_USER, DB_PASSWORD );
+
+            PreparedStatement statement = connection.prepareStatement( 
+                "SELECT * FROM school WHERE country LIKE ?" );
+        ) {
+            statement.setString(1, country);
+
+            try(
+                ResultSet resulSet = statement.executeQuery();
+
+            ) {
+                List<School> schools = new ArrayList<School>();
+
+                while(resulSet.next()){
+                    int id = resulSet.getInt("id");
+                    String name = resulSet.getString("name");
+                    int capacity = resulSet.getInt("capacity");
+                    country = resulSet.getString("country");
+                    schools.add(new School(id, name, capacity, country));
+                }
+
+                return schools;
+            }
+        }
+        catch (SQLException e) {
+            throw new ResponseStatusException( HttpStatus.INTERNAL_SERVER_ERROR, "", e );
+        }
+    }
+
+    class School {
+
+        private int id;
+        private String name;
+        private int capacity;
+        private String country;
+
+        public School(int id, String name, int capacity, String country) {
+            this.id = id;
+            this.name = name;
+            this.capacity = capacity;
+            this.country = country;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getCapacity() {
+            return capacity;
+        }
+
+        public void setCapacity(int capacity) {
+            this.capacity = capacity;
+        }
+
+        public String getCountry() {
+            return country;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+
+    }
+}
